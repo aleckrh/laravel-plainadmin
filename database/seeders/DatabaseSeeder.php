167de17f6b4ea3fdbb6dd6c2cd061e6d183595fc
Alejandro RH <alejandro.rohu@gmail.com>
2024-03-12 10:15:20 -0500

    :rocket: Upgrade to Laravel 11

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+         \App\Models\User::factory()->create([
+             'name' => 'John Doe',
+             'email' => 'test@example.com',
+         ]);
     }
 }
